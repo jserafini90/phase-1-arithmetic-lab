ff81d00f6255120099a06c7dedb52ffe447e7e5f
Jessica Serafini <jpitts_kiheihs@yahoo.com>
2022-06-14 14:02:24 -0400

    commit tree fb2e127fc1ec91da5e25186933a2de59d1ede24f

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let num1 = 15.5;
+let num2 = 4;
+let multiply = num1 * num2;
 
-// Write your code here
+multiply;
+
+let random = 10
+
+Math.random();
+
+const num3 = 24
+const num4 = 6
+let mod = num3 / num4
+
+mod
+
+let max = 20
+
+Math.max(5, 10, 15, 20)
